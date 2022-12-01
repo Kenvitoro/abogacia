fe050a4046a22002b45d5592c5a5c7fe3a080d26
Kenneth Villatoro <kenvittoro@gmail.com>
2022-12-01 00:23:58 -0600

    nav-bar responsive terminado

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const d = document;
+
+/*#################MENU RESPONSIVO####################*/
+
+const bars = d.querySelector(".bars");
+const menu = d.querySelector(".menu")
+const navX = d.querySelector(".fa-x")
+
+bars.addEventListener("click", ()=>{
+      menu.classList.add("nav-active") 
+})
+
+navX.addEventListener("click",()=>{
+    menu.classList.remove("nav-active")
+})
 
